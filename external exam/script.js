477bc1f60a3331cc97372f48604fc6339d853289
Rudra Pratap Singh <rudra.25b01011249@abes.ac.in>
2026-06-19 13:10:39 +0530

    external exam

--- external exam/script.js
+++ external exam/script.js
@@ -1,36 +1,27 @@
-function calculateGrade() {
-    let n = parseInt(prompt("Enter number of students:"));
-    let result = "";
+function calculate() {
+    let total = 0;
 
-    for (let i = 1; i <= n; i++) {
-        let m1 = parseFloat(prompt(`Enter Subject 1 marks for Student ${i}`));
-        let m2 = parseFloat(prompt(`Enter Subject 2 marks for Student ${i}`));
-        let m3 = parseFloat(prompt(`Enter Subject 3 marks for Student ${i}`));
-
-        let total = m1 + m2 + m3;
-        let avg = total / 3;
-
-        let grade;
-
-        if (avg >= 90)
-            grade = "A";
-        else if (avg >= 75)
-            grade = "B";
-        else if (avg >= 60)
-            grade = "C";
-        else if (avg >= 40)
-            grade = "D";
-        else
-            grade = "F";
-
-        result += `
-            <h3>Student ${i}</h3>
-            Total: ${total}<br>
-            Average: ${avg.toFixed(2)}<br>
-            Grade: ${grade}
-            <hr>
-        `;
+    for (let i = 1; i <= 4; i++) {
+        let marks = Number(prompt("Enter marks of Subject " + i));
+        total += marks;
     }
 
-    document.getElementById("output").innerHTML = result;
+    let avg = total / 4;
+    let grade;
+
+    if (avg >= 90)
+        grade = "A";
+    else if (avg >= 75)
+        grade = "B";
+    else if (avg >= 60)
+        grade = "C";
+    else if (avg >= 40)
+        grade = "D";
+    else
+        grade = "F";
+
+    document.getElementById("result").innerHTML =
+        "Total Marks: " + total + "<br>" +
+        "Average Marks: " + avg.toFixed(2) + "<br>" +
+        "Grade: " + grade;
 }
